797007014d60e59e178be618b17cc0adbfdd4863
Andrey <paska-andrej@yandex.ru>
2020-05-15 18:47:43 +0300

    Feedback slider test

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.feed__carousel').slick({
+        centerMode: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.png"></button>',
+    });
+
+});
